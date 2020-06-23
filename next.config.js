a8acc1a826eb8cadfbf159f9b60dbb3d0c38b790
Nicolai Mølgaard Christensen <nmchr7@gmail.com>
2020-06-23 13:22:23 +0200

    - Add PWA support. - Fix manifest and add the correct icons

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
-// Use the hidden-source-map option when you don't want the source maps to be
-// publicly available on the servers, only to the error reporting
 const withSourceMaps = require('@zeit/next-source-maps')();
-
-// Use the SentryWebpack plugin to upload the source maps during build step
 const SentryWebpackPlugin = require('@sentry/webpack-plugin');
+const withPWA = require('next-pwa');
 
 const {
   NEXT_PUBLIC_SENTRY_DSN: SENTRY_DSN,
@@ -15,32 +12,12 @@ const {
 
 process.env.SENTRY_DSN = SENTRY_DSN;
 
-module.exports = withSourceMaps({
+const nextConfig = {
   webpack: (config, options) => {
     const copy = config;
-    // In `pages/_app.js`, Sentry is imported from @sentry/node. While
-    // @sentry/browser will run in a Node.js environment, @sentry/node will use
-    // Node.js-only APIs to catch even more unhandled exceptions.
-    //
-    // This works well when Next.js is SSRing your page on a server with
-    // Node.js, but it is not what we want when your client-side bundle is being
-    // executed by a browser.
-    //
-    // Luckily, Next.js will call this webpack function twice, once for the
-    // server and once for the client. Read more:
-    // https://nextjs.org/docs/api-reference/next.config.js/custom-webpack-config
-    //
-    // So ask Webpack to replace @sentry/node imports with @sentry/browser when
-    // building the browser's bundle
     if (!options.isServer) {
       copy.resolve.alias['@sentry/node'] = '@sentry/browser';
     }
-
-    // When all the Sentry configuration env variables are available/configured
-    // The Sentry webpack plugin gets pushed to the webpack plugins to build
-    // and upload the source maps to sentry.
-    // This is an alternative to manually uploading the source maps
-    // Note: This is disabled in development mode.
     if (
       SENTRY_DSN &&
       SENTRY_ORG &&
@@ -60,4 +37,28 @@ module.exports = withSourceMaps({
 
     return copy;
   },
-});
+
+  pwa: {
+    dest: 'public',
+    disable: process.env.NODE_ENV === 'development',
+    runtimeCaching: [
+      {
+        urlPattern: /^https?.*/,
+        handler: 'NetworkFirst',
+        options: {
+          cacheName: 'https-calls',
+          networkTimeoutSeconds: 15,
+          expiration: {
+            maxEntries: 150,
+            maxAgeSeconds: 30 * 24 * 60 * 60, // 1 month
+          },
+          cacheableResponse: {
+            statuses: [0, 200],
+          },
+        },
+      },
+    ],
+  },
+};
+
+module.exports = withSourceMaps(withPWA(nextConfig));
--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -31,8 +31,7 @@ export default class MainDocument extends Document {
       <html lang="en">
         <link rel="manifest" href="/manifest.json" />
         <meta name="theme-color" content="#f95700" />
-
-        <link rel="apple-touch-icon" href="/images/icons/icon-192x192.png" />
+        <link rel="apple-touch-icon" href="/icon-180x180.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="96x96" href="/favicon-96x96.png" />
